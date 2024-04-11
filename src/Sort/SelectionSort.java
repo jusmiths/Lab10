6aacf201c84418e19fbf563892348c51206f1d2c
Justin <jusmiths@umich.edu>
2024-04-10 23:49:26 -0400

    Fixed swap logic

--- src/Sort/SelectionSort.java
+++ src/Sort/SelectionSort.java
@@ -18,8 +18,8 @@ public class SelectionSort {
 				if (x[i] > x[j]) {
 					temp = x[i];
 					x[i] = x[j];
-					temp = x[j];
-				}
+					x[j] = temp; // Corrected swap logic
+				}				
 			} // end of inner for loop
 		} // end of outer for loop
 		return x;
